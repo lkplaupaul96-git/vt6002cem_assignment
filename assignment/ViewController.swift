0f8b8265f4ab2b0be34c3af0d8326e63b8fbd8db
Paul Lau <lkplaupaul96.git@gmail.com>
2022-01-09 15:08:32 +0800

    Initial Commit

---
+++ assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment
+//
+//  Created by Paul Lau on 9/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
