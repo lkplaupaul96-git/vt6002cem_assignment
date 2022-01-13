e5f7c6fe777f92dd732953a1b43785bc72616d4b
Paul Lau <lkplaupaul96.git@gmail.com>
2022-01-13 21:46:04 +0800

    added gallery view

---
+++ assignment/Views/GalleryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GalleryTableViewCell.swift
+//  assignment
+//
+//  Created by Paul Lau on 13/1/2022.
+//
+
+import UIKit
+
+class GalleryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
